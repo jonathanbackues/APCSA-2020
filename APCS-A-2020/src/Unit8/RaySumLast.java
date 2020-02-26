f99770295d7d2b07687573e6bdcecf5244eec783
jonathanbackues <60591207+jonathanbackues@users.noreply.github.com>
2020-02-26 11:41:24 -0800

    Unit8

---
+++ APCS-A-2020/src/Unit8/RaySumLast.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+package Unit8;
+
+public class RaySumLast {
+	//static int sum = 0;
+
+	public static int go(int[] ray) {
+		int sum = 0;
+		if (ray.length <= 1) {
+			return -1;
+		}
+		for (int i = 0; i < ray.length-1; i++) {
+			if (ray[i] > ray[ray.length - 1]) {
+				sum += ray[i];
+			}
+		}
+
+		if(sum>0 ) {
+			return sum;
+		} else {
+			return -1;
+		}
+	}
+}  
